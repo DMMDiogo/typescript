344fb3c939ef79f7a80efcf2ab136f8ce20c4b1b
Diogo <diogo.vpa@gmail.com>
2024-06-12 21:01:01 +0100

    Added typescrit exercise made on class

---
+++ types.ts
@@ -0,0 +1,11 @@
+let isLoading: boolean = true;
+
+const x: number = 1;
+
+const msg: string = "Hello World";
+const arr2: number[] = [1, 2, 3];
+
+console.log(arr2)
+
+const tuple: [string, number] = ["hello", 10];
+type Tuple = [string, number, boolean]
